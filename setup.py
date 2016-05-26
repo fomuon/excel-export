c86803eec43e32c360fa819cc669e1f4f469d8cd
yongkyu <yongkyu@piensoft.co.kr>
2016-05-26 16:35:27 +0900

    setup 정보 수정

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env
 # encoding: utf-8
-from setuptools import setup
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
 
 try:
-	with open('README.rst') as f:
+	with open('README.md') as f:
 		readme = f.read()
 except IOError:
 	readme = ''
 
+from excel_export import __version__
 setup(
 	name="excel-export",
-	version='0.1.0',
+	version=__version__,
+	packages = ['excel_export'],
+	package_dir={'excel_export': 'excel_export'},
+	install_requires=["xlrd"],
+	license = 'BSD',
+	
 	author='Lee,Yongkyu',
 	author_email='fomuon@gmail.com',
 	url='https://github.com/fomuon/excel-export',
 	entry_points = { 'console_scripts': ['excel-export = excel_export.cli:entry_point'] },
-	packages = ['excel_export'],
 	description="A command-line tool (and python library) to extract sqlite db from excel files",
 	long_description=readme,
-	license = 'BSD',
-	install_requires=["xlrd"],
+	keywords=['excel-export', 'excel', 'sqlite']
 )
