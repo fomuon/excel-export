be6cfad9511f17fbbc66b21e279caeeddaa4dee2
yongkyu <yongkyu@piensoft.co.kr>
2016-06-09 18:53:09 +0900

    db 파일 생성을 option으로 처리

--- excel_export/cli.py
+++ excel_export/cli.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import sys
 import argparse
 import os
@@ -7,14 +7,11 @@ import extract, export
 from excel_export import __version__
 
 def main(argv):
-	parser = argparse.ArgumentParser('excel-extract_tables', 'Extract Sql (mysql or sqlite) from specifically formatted excel file')
+	parser = argparse.ArgumentParser('excel-extract_tables', 'Extract sql or db(sqlite) from specifically formatted excel file')
 	
 	parser.add_argument('-I', '--input-files', nargs='+', type=str, help='excel file path to extract sql. -I file1.xlsx file2.xlsx')
 	parser.add_argument('-O', '--output-dir', default=os.getcwd(), help='output directory. default current directory')
-# 	parser.add_argument('-T', '--output-type', default='all', choices=['mysql', 'sqlite', 'all'])
-# 	parser.add_argument('--without-mysql-ddl', default=False, help='generate create table statement for mysql')
-# 	parser.add_argument('--without-sqlite-ddl', default=False, help='generate create table statement for sqlite')
-# 	parser.add_argument('--without-sqlite-db', default=False, help='create sqlite db file')
+	parser.add_argument('--with-db-file', default=False, action='store_true', help='create sqlite db file')
 	parser.add_argument('--version', default=False, action='store_true', help='Print the current version')
 	
 	args = parser.parse_args()
@@ -25,13 +22,25 @@ def main(argv):
 	
 	if args.input_files:
 		for excel_file in args.input_files:
+			excel_file = unicode(excel_file, sys.getfilesystemencoding())
+			
 			tables = extract.extract_tables_from_excel(excel_file)
+			
+			sqls = export.convert_to_sqls(tables)
+			
 			file_name, file_ext = os.path.splitext(os.path.basename(excel_file))  # @UnusedVariable
-			output_files = export.export_to_sqlite3(tables, args.output_dir, file_name)
-		
+			
 			print excel_file, "===>"
-			for file_path in output_files:
-				print "\t", file_path
+			
+			sql_file = os.path.join(args.output_dir, file_name + ".sql")
+			export.export_to_sqlfile(sqls, sql_file)
+			print "\t", sql_file
+			
+			if args.with_db_file:
+				db_file = os.path.join(args.output_dir, file_name + ".db")
+				export.export_to_sqlite3(sqls, db_file)
+				print "\t", db_file
+		
 		print "Done!"
 	else:
 		parser.print_help()
