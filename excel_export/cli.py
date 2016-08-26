650b9d469fe247985400ff6d312297b84653944f
ykmini <ykmini@ykmini-PC>
2016-08-26 23:33:57 +0900

    추출 제외 정보 인자 파싱 처리

--- excel_export/cli.py
+++ excel_export/cli.py
@@ -13,12 +13,17 @@ def main(argv):
 	parser.add_argument('-O', '--output-dir', default=os.getcwd(), help='output directory. default current directory')
 	parser.add_argument('--with-mysql', default=False, action='store_true', help='create sql file for mysql')
 	parser.add_argument('--with-db-file', default=False, action='store_true', help='create sqlite db file')
+	parser.add_argument('--sqlite-exclude', default=None, type=str, help='[REMOVE_TAB_NAME],..., [TAB_NAME(REMOVE_COLUMN_NAME,...,)]')
 	parser.add_argument('--version', default=False, action='store_true', help='Print the current version')
 	
 	args = parser.parse_args()
 	
 	if args.version:
 		print('excel-export version {}'.format(__version__))
+		exit(0)
+	
+	if args.sqlite_exclude:
+		_parse_exclude_arg(args.sqlite_exclude)
 		exit(0)
 	
 	if args.input_files:
@@ -52,6 +57,34 @@ def main(argv):
 	else:
 		parser.print_help()
 	
+	pass
+
+#추출시 제외할 정보 파서
+#return (제외한 테이블, 제외할 컬럼) ( set(except_table), { table : set(except_cols) } )
+def _parse_exclude_arg(exclude_arg):
+	exclude_tables = set([])
+	exclude_cols = {}
+	
+	is_in_tab = None
+	
+	for info in exclude_arg.split(','):
+		if '(' in info:
+			tmp = info.split('(')
+			exclude_cols[tmp[0]] = set([tmp[1].strip()])
+			is_in_tab = tmp[0]
+		elif ')' in info:
+			exclude_cols[is_in_tab].add(info.split(')')[0].strip())
+			is_in_tab = None
+		elif is_in_tab:
+			exclude_cols[is_in_tab].add(info.strip())
+		else:
+			exclude_tables.add(info.strip())
+			
+	
+	print exclude_tables
+	print exclude_cols
+	
+	return (exclude_tables, exclude_cols)
 	pass
 
 def entry_point():
