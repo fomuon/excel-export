113792029598c9f1e3ca2a5d4603bd6d9a3c2e28
yongkyu <yongkyu@piensoft.co.kr>
2016-08-30 16:15:02 +0900

    추출 기능 개선
    추출 제외 기능
    truncate 구문 추가 가능

--- excel_export/cli.py
+++ excel_export/cli.py
@@ -2,18 +2,26 @@
 # -*- coding: utf-8 -*-
 import sys
 import argparse
-import os
 import extract, export
 from excel_export import __version__
 
 def main(argv):
-	parser = argparse.ArgumentParser('excel-extract_tables', 'Export sql(sqlite, mysql) or db(sqlite) from specifically formatted excel file')
+	parser = argparse.ArgumentParser('excel-extract_tables', 'Export sql(sqlite, mysql) or db(sqlite) from specifically formatted excel file.')
 	
-	parser.add_argument('-I', '--input-files', nargs='+', type=str, help='excel file path to extract sql. -I file1.xlsx file2.xlsx')
-	parser.add_argument('-O', '--output-dir', default=os.getcwd(), help='output directory. default current directory')
-	parser.add_argument('--with-mysql', default=False, action='store_true', help='create sql file for mysql')
-	parser.add_argument('--with-db-file', default=False, action='store_true', help='create sqlite db file')
-	parser.add_argument('--sqlite-exclude', default=None, type=str, help='[REMOVE_TAB_NAME],..., [TAB_NAME(REMOVE_COLUMN_NAME,...,)]')
+	parser.add_argument('-I', '--input-files', nargs='+', type=str, help='Excel file path to export scripts. -I file1.xlsx file2.xlsx')
+	
+	group_sqlite = parser.add_argument_group('[For Sqlite]', 'Options for sqlite exporting.')
+	group_sqlite.add_argument('--sqlite-output', default=None, type=str, help='Output FILE NAME for sqlite script.')
+	group_sqlite.add_argument('--sqlite-add-create-table', default=False, action='store_true', help='Add DROP and CREATE TABLE statement in script file.')
+	group_sqlite.add_argument('--sqlite-with-db-file', default=False, action='store_true', help='Enable Exporting DB file.')
+	group_sqlite.add_argument('--sqlite-exclude', default=None, type=str, help='Add excluding tables and columns. --sqlite-exclude "table1,table2(col1,col2)"')
+	
+	group_mysql = parser.add_argument_group('[For MySql]', 'Options for mysql exporting.')
+	group_mysql.add_argument('--mysql-output', default=None, type=str, help='Output FILE NAME for mysql script.')
+	group_mysql.add_argument('--mysql-add-create-table', default=False, action='store_true', help='Add DROP and CREATE TABLE statement in script file.')
+	group_mysql.add_argument('--mysql-add-truncate', default=False, action='store_true', help='Add TRUNCATE TABLE statement in script file.')
+	group_mysql.add_argument('--mysql-exclude', default=None, type=str, help='Add excluding tables and columns. --sqlite-exclude "table1,table2(col1,col2)"')
+	
 	parser.add_argument('--version', default=False, action='store_true', help='Print the current version')
 	
 	args = parser.parse_args()
@@ -22,38 +30,35 @@ def main(argv):
 		print('excel-export version {}'.format(__version__))
 		exit(0)
 	
-	if args.sqlite_exclude:
-		_parse_exclude_arg(args.sqlite_exclude)
-		exit(0)
-	
-	if args.input_files:
+	if args.input_files and (args.sqlite_output or args.mysql_output):
+		all_tables = {}
+		
 		for excel_file in args.input_files:
 			excel_file = unicode(excel_file, sys.getfilesystemencoding())
+			print excel_file
 			
 			tables = extract.extract_tables_from_excel(excel_file)
+			all_tables.update(tables)
+		
+		if args.sqlite_output:
+			exclude_info = parse_exclude_info(args.sqlite_exclude) if args.sqlite_exclude else None
+			sqls = export.convert_to_sqls_for_sqlite(all_tables, exclude_info=exclude_info, add_create_table=args.sqlite_add_create_table)
 			
-			sqls = export.convert_to_sqls(tables)
+			export.export_to_sqlfile(sqls, args.sqlite_output)
 			
-			file_name, file_ext = os.path.splitext(os.path.basename(excel_file))  # @UnusedVariable
+			if args.sqlite_with_db_file and args.sqlite_add_create_table:
+				export.export_to_sqlite3(sqls, args.sqlite_output + ".db")
 			
-			print excel_file, "===>"
+			pass
+		
+		if args.mysql_output:
+			exclude_info = parse_exclude_info(args.mysql_exclude) if args.mysql_exclude else None
+			sqls = export.convert_to_sqls_for_mysql(all_tables, exclude_info=exclude_info, add_create_table=args.sqlite_add_create_table, add_truncate=args.mysql_add_truncate)
 			
-			sql_file = os.path.join(args.output_dir, file_name + ".sql")
-			export.export_to_sqlfile(sqls, sql_file)
-			print "\t", sql_file
+			export.export_to_sqlfile(sqls, args.mysql_output)
 			
-			if args.with_db_file:
-				db_file = os.path.join(args.output_dir, file_name + ".db")
-				export.export_to_sqlite3(sqls, db_file)
-				print "\t", db_file
-			
-			if args.with_mysql:
-				sql_file = os.path.join(args.output_dir, file_name + "_mysql.sql")
-				sqls = export.convert_to_sqls(tables, 'mysql')
-				export.export_to_sqlfile(sqls, sql_file)
-				print "\t", sql_file
-				
-		print "Done!"
+			pass
+	
 	else:
 		parser.print_help()
 	
@@ -61,34 +66,49 @@ def main(argv):
 
 #추출시 제외할 정보 파서
 #return (제외한 테이블, 제외할 컬럼) ( set(except_table), { table : set(except_cols) } )
-def _parse_exclude_arg(exclude_arg):
+def parse_exclude_info(exclude_arg):
 	exclude_tables = set([])
 	exclude_cols = {}
 	
-	is_in_tab = None
+	last_elem = []
+	in_bracket_tab = None
 	
-	for info in exclude_arg.split(','):
-		if '(' in info:
-			tmp = info.split('(')
-			exclude_cols[tmp[0]] = set([tmp[1].strip()])
-			is_in_tab = tmp[0]
-		elif ')' in info:
-			exclude_cols[is_in_tab].add(info.split(')')[0].strip())
-			is_in_tab = None
-		elif is_in_tab:
-			exclude_cols[is_in_tab].add(info.strip())
+	for ch in exclude_arg:
+		if ch == '(':
+			in_bracket_tab = ''.join(last_elem).strip()
+			if in_bracket_tab:
+				exclude_cols[in_bracket_tab] = set([])
+			last_elem = []
+		elif ch == ')':
+			if in_bracket_tab:
+				v = ''.join(last_elem).strip()
+				if v:
+					exclude_cols[in_bracket_tab].add(v)
+				
+			last_elem = []
+			in_bracket_tab = None
+		elif ch == ',':
+			v = ''.join(last_elem).strip()
+			if v:
+				if in_bracket_tab:
+					exclude_cols[in_bracket_tab].add(v)
+				else:
+					exclude_tables.add(v)
+				
+			last_elem = []
 		else:
-			exclude_tables.add(info.strip())
-			
+			last_elem.append(ch)
 	
-	print exclude_tables
-	print exclude_cols
+	v = ''.join(last_elem).strip()
+	if v:
+		exclude_tables.add(v)
+	
 	
 	return (exclude_tables, exclude_cols)
-	pass
 
 def entry_point():
 	main(sys.argv[1:])
 
 if __name__ == "__main__":
 	entry_point()
+	
